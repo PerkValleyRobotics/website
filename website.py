ce87835be42b19d2dbad3be65b4b8653073310c6
Peter Spychalsky <peterspy2002@outlook.com>
2020-07-18 13:28:22 -0400

    Added an example html file and fixed running the python

--- website.py
+++ website.py
@@ -3,7 +3,8 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
-
+def landing_page() -> "html":
+    return "temp"
 
 if __name__ == "__main__":
     app.run(debug=True)
