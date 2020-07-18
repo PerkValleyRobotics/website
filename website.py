a5ec0df349c87d15ee2e781c788bae47c703b8cd
jolly2014 <68126964+jolly2014@users.noreply.github.com>
2020-07-18 13:55:36 -0400

    Added under construction sign

--- website.py
+++ website.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
-def landing_page(): # Returns html
-    return "temp"
+def home_page(): # Returns html
+    return render_template("entry.html", the_title="Perkiomen Valley Robotics Scouting Page")
 
 if __name__ == "__main__":
     app.run(debug=True)
