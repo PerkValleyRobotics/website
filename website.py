cdc7505a970f75a3abbd719d4e200646cd8837c3
jolly2014 <68126964+jolly2014@users.noreply.github.com>
2020-07-18 17:54:24 -0400

    Added functionality to every page

--- website.py
+++ website.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home_page(): # Returns html
     return render_template("home.html", the_title="PV Robotics Home")
 
+@app.route("/About")
+def about_page(): # Returns html
+    return render_template("about.html", the_title="PV Robotics About")
+
+@app.route("/Calendar")
+def calendar_page(): # Returns html
+    return render_template("calendar.html", the_title="PV Robotics Calendar")
+
+@app.route("/Tasks")
+def task_page(): # Returns html
+    return render_template("task.html", the_title="PV Robotics Tasks")
+
+@app.route("/CodeTeamBestTeam")
+def code_best():
+    return render_template("home.html", the_title="Code Team Best Team")
+
 if __name__ == "__main__":
     app.run(debug=True)
