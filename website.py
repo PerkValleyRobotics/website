b306dc06268a8f4e6146cfd0eccc18646a137acf
jolly2014 <68126964+jolly2014@users.noreply.github.com>
2020-07-18 20:12:35 -0400

    Added updates and posting

--- website.py
+++ website.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+
+class updateForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    text = StringField('Text', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+posts = []
+
 
 @app.route("/")
 def home_page(): # Returns html
@@ -17,6 +31,19 @@ def calendar_page(): # Returns html
 @app.route("/Tasks")
 def task_page(): # Returns html
     return render_template("task.html", the_title="PV Robotics Tasks")
+
+@app.route("/Updates")
+def updates_page(): # Returns html
+    return render_template('updates.html', posts=posts, the_title="PV Robotics Updates")
+
+@app.route('/Post', methods=["GET","POST"])
+def login():
+    form = updateForm()
+    if form.validate_on_submit():
+        flash('Posted')
+        posts.append({"author" : form.name.data, "body" : form.text.data})
+        return redirect('/Updates')
+    return render_template('post.html', title='Sign In', form=form)
 
 @app.route("/CodeTeamBestTeam")
 def code_best():
